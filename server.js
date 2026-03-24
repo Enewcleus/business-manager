feac6cc08833f7c3755fe06d1f86c5fb94cc0b20
Enewcleus <piyush@enewcleus.in>
2026-03-24 12:48:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.use('/api/notifications',   require('./routes/notifications'));
 app.use('/api/users',           require('./routes/users'));
 app.use('/api/dsr',             require('./routes/dsr'));
 app.use('/api/close-requests',  require('./routes/close_requests'));
+app.use('/api/settings',        require('./routes/settings'));  // ✅ NEW
 
 app.use('/api/hurdles',         require('./routes/allroutes').hurdleRouter);
 
